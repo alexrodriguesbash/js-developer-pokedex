b99e82e7a0d9ff1683506e3127ad7ed616693be9
alexrodriguesbash <alek.rodrigues@hotmail.com>
2023-10-21 22:47:16 -0300

    incluido o model do pokémon

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,10 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
+       
+
         <li class="pokemon ${pokemon.type}">
+
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -19,7 +22,9 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            </a>
         </li>
+       
     `
 }
 
@@ -45,3 +50,45 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+
+pokemonList.addEventListener('click', async (event) => {
+    let target = event.target;
+    while (target !== null) {
+        if (target.classList.contains('pokemon')) {
+            const pokemonName= target.querySelector('.name').textContent; 
+            const pokemonNumber= target.querySelector('.number').textContent.replace('#', '');      
+            const modalContent = document.getElementById('modalContent');
+
+            // Você pode usar o nome do Pokémon (obtido do elemento com classe "name") para preencher o conteúdo do modal
+            modalContent.innerHTML = `
+                <div class="image-container">
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonNumber}.svg" alt="${pokemonName}">
+                </div>
+
+                <h3>#${pokemonName}</h3>
+                <h3>${pokemonNumber}</h3>
+            `;
+
+            const modal = document.getElementById('pokemonModal');
+            modal.style.display = 'block';
+
+            const closeBtn = document.querySelector('.close');
+            closeBtn.addEventListener('click', () => {
+                modal.style.display = 'none';
+            });
+
+            // Feche o modal se o usuário clicar fora dele
+            window.addEventListener('click', (event) => {
+                if (event.target === modal) {
+                    modal.style.display = 'none';
+                }
+            });
+
+            break; // Sair do loop após encontrar o elemento com a classe "pokemon"
+        }
+        target = target.parentElement; // Verificar elementos pais
+    }
+});
+
+
